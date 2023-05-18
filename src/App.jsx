2aac17f3656d20d7350b0e0c9e605748fb93eb44
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-05-18 15:59:05 -0400

    working on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Header from "./Header"
 
 function App() {
   
   return (
     <>
-   
+    <Header />
     </>
   )
 }
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+
+
+function Header() {
+    return (
+        <div>
+            <div className = "header">
+                why is this text in yehuppitsville
+            </div>
+            <div style={{color: "red"}}>where the hell does this text go</div>
+            <p>why is this like this </p>
+        </div>
+    )
+}
+
+export default Header
