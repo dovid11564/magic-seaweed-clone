aaf745a203f2cba456406cce32b133b64cfc9e4b
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-05-23 11:07:53 -0400

    config to handle env vars

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+
+export default ({ command, mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return defineConfig({
+        plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+        server: {
+            watch: {
+                usePolling: true,
+            },
+            host: true,
+            strictPort: true,
+        },
+        define: {
+            __APP_ENV__:  JSON.stringify('env.REACT_APP_DATE_TIME_KEY'),
+        }
+    });
+}
