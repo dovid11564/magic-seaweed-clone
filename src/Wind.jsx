037c3d980d155cb9fd1a2d19308949be9df42e5d
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-05-30 15:08:33 -0400

    formatted code cleanly

--- src/Wind.jsx
+++ src/Wind.jsx
@@ -1,15 +1,15 @@
 
 
 function Wind() {
-    return(
+    return (
         <>
-        <div className='border'>
-            Wind Component
-            <p className='border-1'>Render Wind Speed:</p>
-            <p className='border-2'>Render Onshore?:</p>
-            <p className='border-1'>Render Compass Direction:</p>
-            <p className='border-2'>Render Silly Little Arrow Guy:</p>
-        </div>
+            <div className='border'>
+                Wind Component
+                <p className='border-1'>Render Wind Speed:</p>
+                <p className='border-2'>Render Onshore?:</p>
+                <p className='border-1'>Render Compass Direction:</p>
+                <p className='border-2'>Render Silly Little Arrow Guy:</p>
+            </div>
         </>
     )
 }
