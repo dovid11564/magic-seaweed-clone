3ccc36e295929f75f8cf44f4445c161e5bd7dbf8
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-05-30 17:03:38 -0400

    refactored code with ref to scope

--- src/Time.jsx
+++ src/Time.jsx
@@ -1,10 +1,24 @@
-
+import { useState, useEffect } from 'react'
 
 function Time() {
 
+    const [dateTime, setDateTime] = useState(null)
+    useEffect(() => {
+        // this fetch grabs the current time for the beach being rendered.
+        // in order to make it more dynamic, will need to do some kind of interpolation for city or coordinates. 
+        // for now, it just staticlly grabs for NY, since that is where Rockaway Beach is located. 
+        fetch(
+            `http://api.timezonedb.com/v2.1/get-time-zone?key=${import.meta.env.VITE_REACT_APP_DATE_TIME_KEY}&format=json&by=zone&zone=America/New_York`
+        )
+            .then(res => res.json())
+            .then(data => setDateTime(data))
+    }, [])
+
+    // console.log("test", dateTime?.formatted)
+    console.log(dateTime);
     return (
         <>
-            <p className='border-2'>Render Local Date/Time:</p>
+            <p className='border-2'>Render Local Date/Time: {dateTime?.formatted} </p>
         </>
     )
 }
