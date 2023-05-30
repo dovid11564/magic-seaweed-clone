223e6cfe5b31d89608ad8d8b5daeb74cd050e2c9
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-05-30 15:07:07 -0400

    refactored Header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,14 @@
-
+import Time from './Time';
+import Name from './Name';
 
 function Header() {
     return (
         <>
             <div className='border'>
                 Header Component
-                <p className='border-1'>Render Beach Name:</p>
-                <p className='border-2'>Render Local Date/Time:</p>
+                {/* <p className='border-1'>Render Beach Name:</p> */}
+                <Name />
+                <Time />
             </div>
         </>
     )
