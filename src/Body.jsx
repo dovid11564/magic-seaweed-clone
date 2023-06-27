918f8f3d3ce54e94a98707bf7171e674094227df
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-06-27 16:16:10 -0400

    initial flexbox begun. troubleshooting

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,15 +1,15 @@
+import "./Body.css"
 import Wind from './pages/overview/Wind'
 import Waves from './pages/overview/Waves'
 import Tempurature from './pages/overview/Tempurature'
 
 function Body() {
     return (
-        <div className='border'>
+        <div className='body'>
             <p>body component</p>
             <Waves />
             <Wind />
             <Tempurature />
-
         </div>
     )
 }
