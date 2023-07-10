e7cb5115360b447ca03a8b128ea23ecfb0fa1372
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-07-10 13:56:10 -0400

    fixed random floating button

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -25,10 +25,6 @@ function Header() {
             <MapPinIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
             Current Weather Condition
           </div>
-        </div>
-      </div>
-      <div className="mt-5 flex lg:ml-4 lg:mt-0">
-        <span className="sm:ml-3">
           <button
             type="button"
             className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -36,7 +32,11 @@ function Header() {
             <HeartIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
             Favorite
           </button>
-        </span>
+        </div>
+      </div>
+      <div className="mt-5 flex lg:ml-4 lg:mt-0">
+        {/* <span className="sm:ml-3">
+        </span> */}
       </div>
       <div className="flex-item-last">
         <Modal />
