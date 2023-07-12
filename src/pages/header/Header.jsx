d0c50db8e8b6274acd91b59f637ef9b5e60a216c
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-07-12 15:36:50 -0400

    Overview tab basic styled

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -11,17 +11,18 @@ import Modal from './Modal'
 
 function Header() {
   return (
-    <div className="lg:flex flex-col lg:items-center lg:justify-between">
+    <div className="flex-col">
+    <div className="flex justify-between">
       <div className="min-w-0 flex-1">
         <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
           Beach Name Goes Here
         </h2>
         <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
           <div className="mt-2 flex items-center text-sm text-gray-500">
-            <ClockIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+            <ClockIcon className="mr-1.5 h-5 w-5  flex-shrink-0 text-gray-400" aria-hidden="true" />
             Date/Time Goes Here
           </div>
-          <div className="mt-2 flex items-center text-sm text-gray-500">
+          <div className="mt-2 flex flex-1 items-center text-sm text-gray-500">
             <MapPinIcon className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
             Current Weather Condition
           </div>
@@ -38,10 +39,11 @@ function Header() {
         {/* <span className="sm:ml-3">
         </span> */}
       </div>
-      <div className="flex-item-last">
-        <Modal />
-      </div>
     </div>
+    <div className="flex-item-last">
+    <Modal />
+  </div>
+  </div>
   )
 }
 
