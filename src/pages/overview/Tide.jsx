f60a543d9240ec27bca38a1a6489547e559f5641
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-07-19 16:51:18 -0400

    leaving off for now to work on caching

--- src/pages/overview/Tide.jsx
+++ src/pages/overview/Tide.jsx
@@ -23,10 +23,11 @@ ChartJS.register(
 
 function Tide() {
 
+    const [tide, setTide] = useState(null)
     const day = "today" //can I keep this in an array? would that transfer nicely? maybe as some kind of state change
     const exampleNum = 4
     const data = {
-        labels: ['CT', `${day}`, 'CT + 2', 'CT + 3', 'CT + 4', 'CT + 5', 'CT + 6', 'CT + 7', 'CT + 8', 'CT + 9', 'CT + 10', 'CT + 11', 'CT + 12'],
+        labels: [`${tide}`, `${day}`, 'CT + 2', 'CT + 3', 'CT + 4', 'CT + 5', 'CT + 6', 'CT + 7', 'CT + 8', 'CT + 9', 'CT + 10', 'CT + 11', 'CT + 12'],
         datasets: [
             {
                 label: 'Tide over next day',
@@ -52,22 +53,24 @@ function Tide() {
         }
     }
 
-    const [tide, setTide] = useState(null)
+    
     const lat = "40.5891"
     const lng = "-73.8010"
     //the following two need to be in year-month-day format
     const today = "today"
     const yomayim = "in two days"
     //send a request in postman to figure out what the data is we need. then learn how to cache. 
-    // useEffect(() => {
-    //     fetch(`https://api.stormglass.io/v2/tide/sea-level/point?lat=${lat}&lng=${lng}&start=2020-02-24&end=2020-02-25`, {
-    //         headers: {
-    //             'Authorization': `${import.meta.env.VITE_REACT_APP_STORMGLASS}`
-    //         }
-    //     })
-    //     .then(res => res.json())
-    //     .then(data => console.log(data))
-    // })
+    useEffect(() => {
+        fetch(`https://api.stormglass.io/v2/tide/sea-level/point?lat=${lat}&lng=${lng}&start=2020-02-24&end=2020-02-25`, {
+            headers: {
+                'Authorization': `${import.meta.env.VITE_REACT_APP_STORMGLASS}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => setTide(data))
+    }, [])
+
+    console.log(tide)
     return (
         <div> this is tide component
             <div>
